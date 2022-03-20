696eabb4f573d2da14cc2f7fd03499adfdc7746a
Riya Nigam <97540322+Riyanigam-1234@users.noreply.github.com>
2022-03-20 12:23:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+//ASSIGMENT2
 
+function fibonacci(n) {
+    let a=0;
+    let b =1;
+    let z;
+    let i = 0;
+    console.log('fibonacci series are:');
+    for(i=0; i<=n; i++) {
+        console.log(a);
+        z = a + b;
+        a=b;
+        b=z;
+    }
+}
+console.log(fibonacci(8));
