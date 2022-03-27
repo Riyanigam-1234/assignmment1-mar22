89e8a5f715dca6f13f6cef7d0a6360a580d015f5
Riya Nigam <97540322+Riyanigam-1234@users.noreply.github.com>
2022-03-27 14:58:09 +0530

    Add files via upload

--- JavaScript/test 27 March/index3.js
+++ JavaScript/test 27 March/index3.js
@@ -1,1 +1,29 @@
+var library = [ 
+    {
+        author: 'Bill Gates',
+        title: 'The Road Ahead',
+        readingStatus: true
+    },
+    {
+        author: 'Steve Jobs',
+        title: 'Walter Isaacson',
+        readingStatus: true
+    },
+    {
+        author: 'Suzanne Collins',
+        title:  'Mockingjay: The Final Book of The Hunger Games', 
+        readingStatus: false
+    }
+ ];
 
+ function displayReadingStatus(arr) {
+     for(let i=0; i<arr.length; i++) {
+         let books= "'" + arr[i].title + "'" + ' by ' + arr[i].author + ".";
+         if(arr[i].readingStatus == true){
+             console.log("Read " + books);
+         } else {
+             console.log("not read yet " + books);
+         }
+    }
+ }
+displayReadingStatus(library);
